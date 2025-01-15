0e9d8e83e0cdf39101b355027cec7a645ac5b217
Matthew Shane Loop <matthew_loop@auburn.edu>
2025-01-15 15:11:25 -0600

    Update wells dataset

--- code/02_create_analysis_dataset.R
+++ code/02_create_analysis_dataset.R
@@ -1,6 +1,5 @@
 library(tidyverse)
-library(sf)
-library(stars)
+library(sjlabelled)
 
 # Read in private well raster data
 
@@ -9,15 +8,21 @@ library(stars)
 mortality <- read_rds(file = "/Volumes/Projects/usgs_cvd_wells_al/data/clean/01_clean_adph_census_primary_cod.rds")
 
 # Join private well data to ADPH mortality CBG
-wells <- read_csv(file = "/Volumes/Projects/usgs_cvd_wells_al/data/raw/2020_wells_20250115.csv") %>%
-  filter(State == "Alabama") # there are only half as many CBGs in as we would expect. THere are 3,924 CBGs in 2020 Census but only 2,072 here. Apparently these estimates are for the 2010 CBGs, not 2020 CBGs, even though the predict well density is for the year 2020.
 
-Hmisc::contents(wells)
+wells_blocks <- read_csv("/Volumes/Projects/usgs_cvd_wells_al/data/raw/Well_Estimates_2020_Blocks.txt")
 
-wells_small <- wells %>%
-  select(GEOID, State, `2020 Housing Units`,`2020 Population`, `% Well Use - 2020`) %>%
-  rename(
-    census_block_group = GEOID
-  )
+wells_block_groups <- wells_blocks %>%
+  filter(State == "AL") %>% 
+  arrange(GEOID_BlockGroup) %>% 
+  group_by(GEOID_BlockGroup) %>% 
+  summarise(mean_pct_wells_cbg = mean(Pct_Wells, na.rm = TRUE)) %>%
+  rename(census_block_group = GEOID_BlockGroup)
+
+df <- mortality %>%
+  left_join(., wells_block_groups) %>%
+  var_labels(mean_pct_wells_cbg = "Percent of housing units relying on private wells in the given CBG. This percent is a mean of the percents across all Census blocks from USEPA/ORD_Water_Source_2020 GitHub repository.")
 
 # Write out the dataset
+
+write_rds(df,
+          file = "/Volumes/Projects/usgs_cvd_wells_al/data/clean/02_analysis_dataset.rds")
