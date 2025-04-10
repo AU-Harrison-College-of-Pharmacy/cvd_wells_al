6741edef64986169fc8f36c6c4d058fbaa8e418e
Matthew Shane Loop <matthew_loop@auburn.edu>
2025-04-10 08:42:14 -0500

    Include percent AA in poisson models

--- code/04_fit_poisson_models.R
+++ code/04_fit_poisson_models.R
@@ -13,23 +13,25 @@ df <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/02_analysis_datas
 df %>%
   mutate(
     cat_centered_scaled_mean_pct_wells = Hmisc::cut2(amt_centered_scaled_mean_pct_wells_cbg, g = 4),
-    cat_centered_scaled_area_land = Hmisc::cut2(amt_centered_scaled_area_land, g = 4)
+    cat_centered_scaled_area_land = Hmisc::cut2(amt_centered_scaled_area_land, g = 4),
+    cat_pct_aa_only = Hmisc::cut2(amt_pct_aa_only, g = 4)
   ) %>%
-  count(cat_centered_scaled_area_land, cat_centered_scaled_mean_pct_wells) %>%
-  arrange(cat_centered_scaled_mean_pct_wells, cat_centered_scaled_area_land) %>%
+  count(cat_centered_scaled_area_land, cat_centered_scaled_mean_pct_wells, cat_pct_aa_only) %>%
+  arrange(cat_centered_scaled_mean_pct_wells, cat_centered_scaled_area_land, cat_pct_aa_only) %>%
   mutate(
-    cat_centered_scaled_mean_pct_wells = as_character(cat_centered_scaled_mean_pct_wells),
-    cat_centered_scaled_area_land = as_character(cat_centered_scaled_area_land)
+    cat_centered_scaled_mean_pct_wells = as.character(cat_centered_scaled_mean_pct_wells),
+    cat_centered_scaled_area_land = as.character(cat_centered_scaled_area_land),
+    cat_pct_aa_only = as.character(cat_pct_aa_only)
   ) %>%
-  tidyr::complete(cat_centered_scaled_area_land, cat_centered_scaled_mean_pct_wells) %>%
+  tidyr::complete(cat_centered_scaled_area_land, cat_centered_scaled_mean_pct_wells, cat_pct_aa_only) %>%
   ggplot(aes(x = cat_centered_scaled_mean_pct_wells, y = n)) +
   geom_bar(stat = "identity") +
-  facet_wrap(~ cat_centered_scaled_area_land, scales = "free_y")
+  facet_grid(cat_pct_aa_only ~ cat_centered_scaled_area_land, scales = "free_y")
 
 df_hypertensive <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_hypertensive_deaths.rds")
 
 f_hypertensive <- lapply(1:length(df_hypertensive$imputations), function(i){
-  f <- glmer(n_hypertensive_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_hypertensive_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_hypertensive$imputations[[i]],
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -44,7 +46,7 @@ write_rds(f_hypertensive,
 df_ischemic <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_ischemic_deaths.rds")
 
 f_ischemic <- lapply(1:length(df_ischemic$imputations), function(i){
-  f <- glmer(n_ischemic_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_ischemic_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_ischemic$imputations[[i]],
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -59,7 +61,7 @@ write_rds(f_ischemic,
 df_stroke_cerebrovascular <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_stroke_cerebrovascular_deaths.rds")
 
 f_stroke_cerebrovascular <- lapply(1:length(df_stroke_cerebrovascular$imputations), function(i){
-  f <- glmer(n_stroke_cerebrovascular_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_stroke_cerebrovascular_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_stroke_cerebrovascular$imputations[[i]],
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -74,7 +76,7 @@ write_rds(f_stroke_cerebrovascular,
 df_diabetes <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_diabetes_deaths.rds")
 
 f_diabetes <- lapply(1:length(df_diabetes$imputations), function(i){
-  f <- glmer(n_diabetes_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_diabetes_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_diabetes$imputations[[i]],
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -96,7 +98,7 @@ df %>%
 
 # Hypertensive
 f_hypertensive_sensitivity_area <- lapply(1:length(df_hypertensive$imputations), function(i){
-  f <- glmer(n_hypertensive_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_hypertensive_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_hypertensive$imputations[[i]] %>% filter(amt_centered_scaled_area_land >= -0.4228, amt_centered_scaled_area_land < 0.0508),
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -108,7 +110,7 @@ write_rds(f_hypertensive_sensitivity_area,
 
 # Ischemic
 f_ischemic_sensitivity_area <- lapply(1:length(df_ischemic$imputations), function(i){
-  f <- glmer(n_ischemic_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_ischemic_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_ischemic$imputations[[i]] %>% filter(amt_centered_scaled_area_land >= -0.4228, amt_centered_scaled_area_land < 0.0508),
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -121,7 +123,7 @@ write_rds(f_ischemic_sensitivity_area,
 # Stroke/cerebrovascular
 
 f_stroke_cerebrovascular_sensitivity_area <- lapply(1:length(df_stroke_cerebrovascular$imputations), function(i){
-  f <- glmer(n_stroke_cerebrovascular_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_stroke_cerebrovascular_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_stroke_cerebrovascular$imputations[[i]] %>% filter(amt_centered_scaled_area_land >= -0.4228, amt_centered_scaled_area_land < 0.0508),
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
@@ -134,7 +136,7 @@ write_rds(f_stroke_cerebrovascular_sensitivity_area,
 # Diabetes
 
 f_diabetes_sensitivity_area <- lapply(1:length(df_diabetes$imputations), function(i){
-  f <- glmer(n_diabetes_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+  f <- glmer(n_diabetes_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg + amt_centered_scaled_area_land + amt_pct_aa_only + offset(log(n_population_times_4)),
              family = poisson(link = "log"),
              data = df_diabetes$imputations[[i]] %>% filter(amt_centered_scaled_area_land >= -0.4228, amt_centered_scaled_area_land < 0.0508),
              glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
