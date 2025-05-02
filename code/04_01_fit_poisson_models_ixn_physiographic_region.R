06ea92f9605b6c577e6bc7126df1a226289b836b
jieunparkmath <jzp0037@auburn.edu>
2025-05-02 14:06:30 -0500

    Update files with inla

--- code/04_01_fit_poisson_models_ixn_physiographic_region.R
+++ code/04_01_fit_poisson_models_ixn_physiographic_region.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(Amelia)
 library(lme4)
 library(sf)
+library(INLA)
 
 #### 
 # Check the covariate overlap of your models
@@ -29,57 +30,59 @@ df %>%
 df_hypertensive <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_hypertensive_deaths.rds")
 
 f_hypertensive <- lapply(1:length(df_hypertensive$imputations), function(i){
-  f <- glmer(n_hypertensive_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
-             family = poisson(link = "log"),
-             data = df_hypertensive$imputations[[i]],
-             glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
+  f <- inla(
+    n_hypertensive_deaths ~ f(id_census_block_group, model = "iid") + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+    data = df_hypertensive$imputations[[i]],
+    family = "poisson",
+    control.compute = list(config = TRUE)
   )
 })
 
+
 write_rds(f_hypertensive, 
-          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_hypertension_deaths_poisson_model_ixn_physiographic_region.rds")
+          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_hypertension_deaths_poisson_model_ixn_physiographic_region_inla.rds")
 
 # Ischemic heart disease
 
 df_ischemic <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_ischemic_deaths.rds")
 
 f_ischemic <- lapply(1:length(df_ischemic$imputations), function(i){
-  f <- glmer(n_ischemic_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
-             family = poisson(link = "log"),
-             data = df_ischemic$imputations[[i]],
-             glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
+  f <- inla(n_ischemic_deaths ~ f(id_census_block_group, model = "iid") + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+            data = df_ischemic$imputations[[i]],
+            family = "poisson",
+            control.compute = list(config = TRUE)
   )
 })
 
 write_rds(f_ischemic, 
-          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_ischemic_deaths_poisson_model_ixn_physiographic_region.rds")
+          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_ischemic_deaths_poisson_model_ixn_physiographic_region_inla.rds")
 
 # Stroke/cerebrovascular
 
 df_stroke_cerebrovascular <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_stroke_cerebrovascular_deaths.rds")
 
 f_stroke_cerebrovascular <- lapply(1:length(df_stroke_cerebrovascular$imputations), function(i){
-  f <- glmer(n_stroke_cerebrovascular_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
-             family = poisson(link = "log"),
-             data = df_stroke_cerebrovascular$imputations[[i]],
-             glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
+  f <- inla(n_stroke_cerebrovascular_deaths ~  f(id_census_block_group, model = "iid") + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
+            data = df_stroke_cerebrovascular$imputations[[i]],
+            family = "poisson",
+            control.compute = list(config = TRUE)
   )
 })
 
 write_rds(f_stroke_cerebrovascular, 
-          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_stroke_cerebrovascular_deaths_poisson_model_ixn_physiographic_region.rds")
+          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_stroke_cerebrovascular_deaths_poisson_model_ixn_physiographic_region_inla.rds")
 
 # Diabetes
 
 df_diabetes <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/03_imputed_diabetes_deaths.rds")
 
 f_diabetes <- lapply(1:length(df_diabetes$imputations), function(i){
-  f <- glmer(n_diabetes_deaths ~ (1 | id_census_block_group) + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
-             family = poisson(link = "log"),
+  f <- inla(n_diabetes_deaths ~  f(id_census_block_group, model = "iid") + cat_age_group + amt_centered_scaled_mean_pct_wells_cbg * cat_physiographic_region + amt_centered_scaled_area_land + offset(log(n_population_times_4)),
              data = df_diabetes$imputations[[i]],
-             glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 500000))
+             family = "poisson",
+             control.compute = list(config = TRUE)
   )
 })
 
 write_rds(f_diabetes, 
-          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_diabetes_deaths_poisson_model_ixn_physiographic_region.rds")
+          "/Volumes/Projects/usgs_cvd_wells_al/output/04_01_diabetes_deaths_poisson_model_ixn_physiographic_region_inla.rds")
