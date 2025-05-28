5b8228fa4672b13222a1f31ba0e8f619eff9d267
Matthew Loop <msl0052@auburn.edu>
2025-05-28 13:10:48 -0500

    Update 10_test_residual_spatial_autocorrelation.R

--- code/10_test_residual_spatial_autocorrelation.R
+++ code/10_test_residual_spatial_autocorrelation.R
@@ -8,8 +8,6 @@ library(tmap)
 
 df <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/data/clean/02_analysis_dataset.rds")
 
-df_st <- 
-
 # Hypertension
 
 f_hypertensive <- read_rds("/Volumes/Projects/usgs_cvd_wells_al/output/04_hypertension_deaths_poisson_model.rds")
