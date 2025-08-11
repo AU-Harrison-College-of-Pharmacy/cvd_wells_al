a7e84b70ec00ea9890ad52526064a50c8ff49dad
jieunparkmath <jzp0037@auburn.edu>
2025-08-11 15:13:48 -0500

    Update pool_predictions_inla().
    The previous version was able to take only two terms. This update can work no matter how many terms we have.

--- code/r/pool_predictions_inla.R
+++ code/r/pool_predictions_inla.R
@@ -6,29 +6,35 @@ pool_predictions_inla <- function(f_condition_inla, terms, condition = list()) {
   
   source("r/predict_response_inla.R")
   source("r/generate_newdata.R")
-  y_hat_stacked <- lapply(f_condition_inla, function(f_condition_inla_each){
-    
-    predict_response_inla(f_condition_inla_each, terms = terms, condition = condition)$y_hat[, -c(1:2)]
-  }) %>% bind_cols() %>%
-    suppressMessages()
   
-  # terms for prediction summary
-  newdata_each <- generate_newdata(f_condition_inla[[1]], terms, condition)
   parsed_terms <- lapply(terms, function(term) {
-    if (grepl("\\[", term)) {
-      var <- sub(" .*", "", term)
-    } else {
-      var <- term
-    }
-  }
-  ) %>% unlist()
+    if (grepl("\\[", term)) sub(" .*", "", term) else term
+  }) %>% unlist()
   
-  # output
-  out <- newdata_each %>% 
-    dplyr::select(all_of(parsed_terms)) %>%
-    mutate(predicted = rowMeans(y_hat_stacked),
-           conf.low = apply(y_hat_stacked, 1, quantile, probs = 0.025),
-           conf.high = apply(y_hat_stacked, 1, quantile, probs = 0.975))
-
+  # Collect numeric draw matrices from each imputation
+  mats <- lapply(f_condition_inla, function(f_condition_inla_each) {
+    yh <- predict_response_inla(
+      f_condition_inla_each,
+      terms = terms,
+      condition = condition
+    )$y_hat
+    
+    k <- length(parsed_terms)                      
+    if (ncol(yh) <= k) stop("No draw columns found in y_hat.")
+    as.matrix(yh[, -(seq_len(k)), drop = FALSE]) 
+  })
+  
+  y_hat_stacked <- do.call(cbind, mats)
+  
+  newdata_each <- generate_newdata(f_condition_inla[[1]], terms, condition)
+  
+  out <- newdata_each %>%
+    dplyr::select(dplyr::all_of(parsed_terms)) %>%
+    dplyr::mutate(
+      predicted = rowMeans(y_hat_stacked),
+      conf.low  = apply(y_hat_stacked, 1, quantile, probs = 0.025, names = FALSE),
+      conf.high = apply(y_hat_stacked, 1, quantile, probs = 0.975, names = FALSE)
+    )
+  
   return(out)
 }
